ffebf87aa87241fdfdd5ced05ae8cf1943a534ea
netoolii <netow@myself.com>
2019-02-24 12:16:22 -0400

    including test

--- test/forever-recording.py
+++ test/forever-recording.py
@@ -1,10 +1,10 @@
-import loop_listen as Loop
+from loop_listen.loop_listen  import Loop_listen as Loop
 
 
 i=0
 try:
     while(True):
-        audio = Loop(filename=str(i), threshold= False, max_seconds=3, rate = 16000, debug=False)
+        audio = Loop(filename=str(i), threshold= False, max_seconds=3, rate = 16000)
         audio.listen()
         i=+1
 except KeyboardInterrupt:
--- test/specific-time.py
+++ test/specific-time.py
@@ -1,10 +1,11 @@
-import loop_listen as Loop
+from loop_listen.loop_listen  import Loop_listen as Loop
+
 
 
 i=5
 try:
     for i in range(i):
-        audio = Loop(filename=str(i), threshold_limit= 5, max_seconds=3, debug=False)
+        audio = Loop(filename=str(i), threshold=False, max_seconds=3)
         audio.listen()
 except KeyboardInterrupt:
     print('interrupted!')
