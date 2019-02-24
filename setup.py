0a72d6fe9ad35a57dec95242e11c149fe9b4337c
netoolii <netow@myself.com>
2019-02-24 11:53:36 -0400

    new version

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from distutils.core import setup
 setup(
     name = 'loop_listen',         # How you named your package folder (MyLib)
     # packages = ['loop_listen'],   # Chose the same as "name"
-    version = '0.1.2',      # Start with a small number and increase it with every change you make
+    version = '0.1.3',      # Start with a small number and increase it with every change you make
     license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description = 'Create a Audio Handle to keep listening your microphone and saving in wav type files',   # Give a short description about your library
     author = 'Wilson Neto',                   # Type in your name
     author_email = 'netow@myself.com',      # Type in your E-Mail
     url = 'https://github.com/netoolii/loop-listen',   # Provide either the link to your github or to your website
-    download_url = 'https://github.com/netoolii/loop-listen/archive/v0.1.2.tar.gz',    # I explain this later on
+    download_url = 'https://github.com/netoolii/loop-listen/archive/v0.1.3.tar.gz',    # I explain this later on
     keywords = ['Audio Loop', 'Listen', 'Open Mic', 'Mic Audio Loop', 'Audio Handle', 'pyaudio', 'wav file', 'Loop Mic'],   # Keywords that define your package best
     install_requires=[            # I get to this in a second
             'numpy',
